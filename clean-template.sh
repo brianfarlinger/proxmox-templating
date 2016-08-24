d270e303c95dd35d0e2afda0da0af012c528f014
Brian Farlinger <brian@lilac.red>
2016-08-24 10:01:31 -0700

    Update clean-template.sh

--- clean-template.sh
+++ clean-template.sh
@@ -2,33 +2,33 @@
 
 #Use this script to clean out UUIDs, log files, etc. Final step of setting up a template
 
-/bin/echo "Now cleaning up files and removing UUIDs"
+echo "Now cleaning up files and removing UUIDs"
 
 #clean up cached yum files
-/usr/bin/yum clean all
+yum clean all
 
 #flush the logs
-/usr/sbin/logrotate –f /etc/logrotate.conf
-/bin/rm –f /var/log/*-???????? /var/log/*.gz
-/bin/rm -f /var/log/dmesg.old
-/bin/rm -rf /var/log/anaconda
-/bin/cat /dev/null > /var/log/audit/audit.log
-/bin/cat /dev/null > /var/log/wtmp
-/bin/cat /dev/null > /var/log/lastlog
-/bin/cat /dev/null > /var/log/grubby
+logrotate –f /etc/logrotate.conf
+rm –f /var/log/*-???????? /var/log/*.gz
+rm -f /var/log/dmesg.old
+rm -rf /var/log/anaconda
+cat /dev/null > /var/log/audit/audit.log
+cat /dev/null > /var/log/wtmp
+cat /dev/null > /var/log/lastlog
+cat /dev/null > /var/log/grubby
 
 #remove temp files
-/bin/rm –rf /tmp/*
-/bin/rm –rf /var/tmp/*
+rm –rf /tmp/*
+rm –rf /var/tmp/*
 
 #remove hardware specific information associated with eth0
-/bin/sed -i ‘/^(HWADDR|UUID)=/d’ /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i ‘/^(HWADDR|UUID)=/d’ /etc/sysconfig/network-scripts/ifcfg-eth0
 
 #remove SSH keys
-/bin/rm –f /etc/ssh/*key*
+rm –f /etc/ssh/*key*
 
 #remove bash history and SSH history from root user. 
-/bin/rm -f ~root/.bash_history
+rm -f ~root/.bash_history
 unset HISTFILE
-/bin/rm -rf ~root/.ssh/
-/bin/rm -f ~root/anaconda-ks.cfg
+rm -rf ~root/.ssh/
+rm -f ~root/anaconda-ks.cfg
